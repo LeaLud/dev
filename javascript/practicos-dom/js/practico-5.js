96166d76d0c2671dc85b3045f3f9aea0c2786a44
Leandro Ludueña <leandro.f.luduena@gmail.com>
2024-01-10 16:57:26 -0300

    update 10-01-2024

--- javascript/practicos-dom/js/practico-5.js
+++ javascript/practicos-dom/js/practico-5.js
@@ -78,9 +78,9 @@ function addTask() {
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
         </svg>`;
 
-        icon2.innerHTML = `<svg id="dropTaskIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="darkslategray" class="bi bi-trash" viewBox="0 0 16 16">
-        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+        icon2.innerHTML = `<svg id="dropTaskIcon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path class="icon2path" d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path class="icon2path" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
         </svg>`;
         
     } else (alert('No se ha introducido algún valor'))
@@ -88,31 +88,25 @@ function addTask() {
 
 let doneTaskIcon = document.getElementById('done-task-icon');
 
-//Definiendo una variable para identificar el estado de reposo de las tareas
-let taskStatus = 'neutral';
-
 function completeTask(e) {
 
-    //Obteniendo el nodo padre del nodo padre del objeto al que se le aplicó la acción
+    //Obteniendo el nodo padre del nodo padre del objeto al que se le aplicó la acción de click
     let currentTask = e.target.parentNode.parentNode;
 
-    //Si el estado de la tarea es el de reposo, se lo pasa a estado chequeado
-    if (taskStatus === 'neutral') { 
-        currentTask.classList.toggle('done-task');
+    //Si el valor de la clase de la tarea es 'task', se le agrega la clase 'done-task' y se cambia el ícono a chequeado
+    if (currentTask.classList.value === 'task') { 
+        currentTask.classList.add('done-task');
         e.target.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-square" viewBox="0 0 16 16">
         <path d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5z"/>
         <path d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0"/>
         </svg>`;
-
-    taskStatus = 'checked';
     } else {
+
+        //Si el valor de la clase no es estrictamente 'task' se le quita la clase 'done-task' y se cambia el ícono a cuadrado sin chequear
         currentTask.classList.remove('done-task');
-        //Si el estado de la tarea es chequeado, se lo vuelve al estado de reposo
         e.target.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16">
         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
         </svg>`;
-
-        taskStatus = 'neutral';
     }
 }
 
@@ -127,12 +121,22 @@ function userKeyDown(e) {
 function dropTask(e) {
 
     //Obteniendo el nodo padre del nodo padre del objeto al que se le aplicó la acción
-    let currentTask = e.target.parentNode.parentNode;
-    currentTask.remove();
+    let currentTask = e.target;
+
+    if (currentTask.classList.value === 'icon') {
+        currentTask = e.target.parentNode;
+        currentTask.remove();
+    } else if (currentTask.id === 'dropTaskIcon') {
+        currentTask = e.target.parentNode.parentNode;
+        currentTask.remove();
+    } else if (currentTask.classList.value === 'icon2path') {
+        currentTask = e.target.parentNode.parentNode.parentNode;
+        currentTask.remove();
+    }
 }
 
 //Definiendo un evento para agregar la tarea al hacer clic sobre el botón +
 buttonAddTask.addEventListener('click', addTask);
 
 //Definiendo un evento para agregar la tarea al presionar Enter
-taskInput.addEventListener('keydown', userKeyDown)
+taskInput.addEventListener('keydown', userKeyDown);
